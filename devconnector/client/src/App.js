1fb8714f9766d262f8cbfa7dc13b6cf768e84ad1
Skye Leahy <skyeleahy@protonmail.com>
2019-03-21 16:27:10 -0700

    add logout & conditional navbar links

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -22,6 +22,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: CLear current Profile
+    // redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
