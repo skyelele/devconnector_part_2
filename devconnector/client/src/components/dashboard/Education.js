1eb45c5dbaf7109a90b0812e91418fca47ae5aba
Skye Leahy <skyeleahy@protonmail.com>
2019-03-22 22:22:25 -0700

    add dashboard education display&delete + add Profiles Component

--- devconnector/client/src/components/dashboard/Education.js
+++ devconnector/client/src/components/dashboard/Education.js
@@ -4,30 +4,30 @@ import PropTypes from "prop-types";
 // You need Moment Library to use
 // Moment from react-moment ;) <3
 import Moment from "react-moment";
-import { deleteExperience } from "../../actions/profileActions";
+import { deleteEducation } from "../../actions/profileActions";
 
 // class-based component
-class Experience extends Component {
+class Education extends Component {
   onDeleteClick(id) {
-    this.props.deleteExperience(id);
+    this.props.deleteEducation(id);
   }
 
   render() {
-    const experience = this.props.experience.map(exp => (
-      <tr key={exp._id}>
-        <td>{exp.company}</td>
-        <td>{exp.title}</td>
+    const education = this.props.education.map(edu => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td>{edu.degree}</td>
         <td>
-          <Moment format="MM/DD/YYYY">{exp.from}</Moment> -
-          {exp.to === null ? (
+          <Moment format="MM/DD/YYYY">{edu.from}</Moment> -
+          {edu.to === null ? (
             " Now"
           ) : (
-            <Moment format="MM/DD/YYYY">{exp.to}</Moment>
+            <Moment format="MM/DD/YYYY">{edu.to}</Moment>
           )}
         </td>
         <td>
           <button
-            onClick={this.onDeleteClick.bind(this, exp._id)}
+            onClick={this.onDeleteClick.bind(this, edu._id)}
             className="btn-btn-danger"
           >
             Delete
@@ -37,28 +37,28 @@ class Experience extends Component {
     ));
     return (
       <div>
-        <h4 className="mb-4">Experience Credentials</h4>
+        <h4 className="mb-4">Education Credentials</h4>
         <table className="table">
           <thead>
             <tr>
-              <th>Company</th>
-              <th>Title</th>
+              <th>School</th>
+              <th>Degree</th>
               <th>Years</th>
               <th />
             </tr>
           </thead>
-          <tbody>{experience}</tbody>
+          <tbody>{education}</tbody>
         </table>
       </div>
     );
   }
 }
 
-Experience.propTypes = {
-  deleteExperience: PropTypes.func.isRequired
+Education.propTypes = {
+  deleteEducation: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  { deleteExperience }
-)(Experience);
+  { deleteEducation }
+)(Education);
